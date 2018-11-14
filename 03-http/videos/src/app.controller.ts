174f8fb405ceb35005a155056de195ca2ccd4b5a
Vicente Adrian Eguez Sarzosa <eadepto@hotmail.com>
2018-11-14 15:52:16 -0500

    controladores

--- 03-http/videos/src/app.controller.ts
+++ 03-http/videos/src/app.controller.ts
@@ -1,12 +1,73 @@
-import { Get, Controller } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Get, Controller, Request, Response,Headers, HttpCode, HttpException, Query, Param} from '@nestjs/common';
+import {AppService} from './app.service';
+import {Observable, of} from "rxjs";
 
-@Controller()
+// http://192.168.1.2:3000/Usuario/saludar     METODO -> GET
+// http://192.168.1.2:3000/Usuario/salir   METODO -> POST
+// http://192.168.1.2:3000/Usuario/registrar METODO -> PUT
+// http://192.168.1.2:3000/Usuario/borrar METODO -> DELETE
+// http://192.168.1.2:3000/Notas
+
+
+// Decorador -> FUNCION
+// SE EJECUTA ANTES DE ALGO
+@Controller('Usuario') // Decoradores!
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  root(): string {
-    return this.appService.root();
-  }
+    @Get('saludar')
+    saludar(
+        @Query() queryParams,
+        @Query('nombre') nombre,
+        @Headers('seguridad') seguridad,
+    ): string { // metodo!
+        return nombre;
+    }
+
+    // /Usuario/segmentoUno/12/segmentoDos
+    @Get('segmentoUno/:idUsuario/segmentoDos')
+    ruta(
+        @Param() todosParametrosRuta,
+        @Param('idUsuario') idUsuario,
+    ): string { // metodo!
+        return idUsuario;
+    }
+
+
+
+
+
+
+
+
+
+
+    @Get('despedirse')
+    @HttpCode(201)
+    despedirse(): Promise<string> {
+        return new Promise<string>(
+            (resolve, reject) => {
+
+                throw new HttpException({
+                        mensaje: 'Error en despedirse',
+                    },
+                    400);
+            }
+        );
+    }
+
+    @Get('tomar')
+    @HttpCode(201)
+    tomar(): string { // metodo!
+        return 'Estoy borracho';
+    }
+
+    @Get('saludarObservable')
+    saludarObservable(): Observable<string> { // metodo!
+        return of('Hola mundo');
+    }
+
+
+
+
+
 }
--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+// const NestFactory = require('@nestjs/core').NestFactory; //js
+import {NestFactory} from '@nestjs/core'; // ts
+// import * as httpserver from 'http-server'; // js
+import {Options} from 'http-server'; // js
+import {AppModule} from './app.module';
+//import {} from './mi-codigo';
+//const a = require('./mi-codigo').a;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    //console.log(a)
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
 }
+
 bootstrap();
+
+// of([1,2,3,4,5])
+// ->map(
+//     ([1,2,3,4,5])=>{
+//         [1,2,3,4,5].findIndex(3)
+//             [1,2,3,4,5][3].asdasdasdasd
+//         return [1,2,3,4,5]
+//     }
+// )
+
