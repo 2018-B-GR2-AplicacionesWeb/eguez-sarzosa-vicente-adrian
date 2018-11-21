afe1b301e72e39a1f54ffad7755ee2f9e2e2378b
Vicente Adrian Eguez Sarzosa <eadepto@hotmail.com>
2018-11-21 15:24:18 -0500

    bdd

--- proyecto/main.js
+++ proyecto/main.js
@@ -1,5 +1,16 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const inquirer = require('inquirer');
 const fs = require('fs');
+const rxjs = require('rxjs');
+const mergeMap = require('rxjs/operators').mergeMap;
+const map = require('rxjs/operators').map;
 const preguntaMenu = {
     type: 'list',
     name: 'opcionMenu',
@@ -11,6 +22,13 @@ const preguntaMenu = {
         'Actualizar',
     ]
 };
+const preguntaBuscarUsuario = [
+    {
+        type: 'input',
+        name: 'idUsuario',
+        message: 'Ingrese ID Usuario',
+    }
+];
 const preguntaUsuario = [
     {
         type: 'input',
@@ -51,24 +69,56 @@ function inicialiarBDD() {
         });
     });
 }
-async function main() {
-    try {
-        const respuestaInicializarBDD = await inicialiarBDD();
-        const bdd = respuestaInicializarBDD.bdd;
-        const respuestaMenu = await inquirer.prompt(preguntaMenu);
-        switch (respuestaMenu.opcionMenu) {
-            case 'Crear':
-                // Preguntar datos del nuevo Usuario
-                const usuario = await inquirer.prompt(preguntaUsuario);
-                // CREAR USUARIO
-                bdd.usuarios.push(usuario);
-                const respuestaGuardado = await guardarBDD(bdd);
-                main();
-                break;
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // 1) Inicializar bdd -- DONE
+        // 2) Preguntas Menu -- DONE
+        // 3) Opciones de Respuesta --  DONE
+        // 4) ACCCION!!!!  -- DONE
+        // 5) Guardar BDD --
+        // of(Cualquier cosa JS)
+        // from(Promesas)
+        const respuestaBDD$ = rxjs.from(inicialiarBDD());
+        respuestaBDD$
+            .pipe(preguntarOpcionesMenu(), opcionesRespuesta(), ejecutarAcccion(), guardarBaseDeDatos())
+            .subscribe((data) => {
+            //
+            console.log(data);
+        }, (error) => {
+            //
+            console.log(error);
+        }, () => {
+            main();
+            console.log('Complete');
+        });
+        /*
+        try {
+            const respuestaInicializarBDD:RespuestaBDD = <RespuestaBDD> await inicialiarBDD();
+            const bdd = respuestaInicializarBDD.bdd;
+            const respuestaMenu = await inquirer.prompt(preguntaMenu);
+    
+            switch (respuestaMenu.opcionMenu) {
+                case 'Crear':
+    
+                    // Preguntar datos del nuevo Usuario
+                    const usuario = await inquirer.prompt(preguntaUsuario);
+    
+                    // CREAR USUARIO
+                    bdd.usuarios.push(usuario); // JS
+    
+                    const respuestaGuardado = await guardarBDD(bdd);
+                    main();
+    
+                    break;
+    
+            }
+    
+        } catch (e) {
+            console.error(e)
         }
-    }
-    catch (e) {
-    }
+    
+        */
+    });
 }
 function guardarBDD(bdd) {
     return new Promise((resolve, reject) => {
@@ -89,3 +139,81 @@ function guardarBDD(bdd) {
     });
 }
 main();
+function preguntarOpcionesMenu() {
+    return mergeMap(// Respuesta Anterior Observable
+    (respuestaBDD) => {
+        return rxjs
+            .from(inquirer.prompt(preguntaMenu))
+            .pipe(map(// respuesta ant obs
+        (respuesta) => {
+            respuestaBDD.opcionMenu = respuesta;
+            return respuestaBDD;
+            // Cualquier cosa JS
+        }));
+        // OBSERVABLE!!!!!!!!!!
+    });
+}
+function opcionesRespuesta() {
+    return mergeMap((respuestaBDD) => {
+        const opcion = respuestaBDD.opcionMenu.opcionMenu;
+        switch (opcion) {
+            case 'Crear':
+                return rxjs
+                    .from(inquirer.prompt(preguntaUsuario))
+                    .pipe(map((usuario) => {
+                    respuestaBDD.usuario = usuario;
+                    return respuestaBDD;
+                }));
+            case 'Buscar':
+                break;
+            case 'Actualizar':
+                return preguntarIdUsuario(respuestaBDD);
+            case 'Borrar':
+                break;
+        }
+    });
+}
+function guardarBaseDeDatos() {
+    return mergeMap(// Respuesta del anterior OBS
+    (respuestaBDD) => {
+        // OBS
+        return rxjs.from(guardarBDD(respuestaBDD.bdd));
+    });
+}
+function ejecutarAcccion() {
+    return map(// Respuesta del anterior OBS
+    (respuestaBDD) => {
+        const opcion = respuestaBDD.opcionMenu.opcionMenu;
+        switch (opcion) {
+            case 'Crear':
+                const usuario = respuestaBDD.usuario;
+                respuestaBDD.bdd.usuarios.push(usuario);
+                return respuestaBDD;
+            case 'Actualizar':
+                const indice = respuestaBDD.indiceUsuario;
+                console.log('respuestaBDD.bdd.usuarios[indice]', respuestaBDD.bdd.usuarios[indice]);
+                return respuestaBDD;
+        }
+    });
+}
+function preguntarIdUsuario(respuestaBDD) {
+    console.log('Entro recursivo')
+    return rxjs
+        .from(inquirer.prompt(preguntaBuscarUsuario))
+        .pipe(map(// RESP ANT OBS
+    (respuesta) => {
+        const indiceUsuario = respuestaBDD.bdd
+            .usuarios
+            .findIndex(// -1
+        (usuario) => {
+            return usuario.id === respuesta.idUsuario;
+        });
+        if (indiceUsuario === -1) {
+            preguntarIdUsuario(respuestaBDD);
+        }
+        else {
+            respuestaBDD.indiceUsuario = indiceUsuario;
+            return respuestaBDD;
+        }
+    }));
+}
--- proyecto/main.ts
+++ proyecto/main.ts
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const rxjs = require('rxjs');
+const mergeMap = require('rxjs/operators').mergeMap;
+const map = require('rxjs/operators').map;
 
 const preguntaMenu = {
     type: 'list',
@@ -13,6 +16,14 @@ const preguntaMenu = {
     ]
 };
 
+const preguntaBuscarUsuario = [
+    {
+        type: 'input',
+        name: 'idUsuario',
+        message: 'Ingrese ID Usuario',
+    }
+];
+
 const preguntaUsuario = [
     {
         type: 'input',
@@ -25,6 +36,7 @@ const preguntaUsuario = [
         message: 'Cual es tu nombre'
     },
 ];
+
 
 function inicialiarBDD() {
 
@@ -69,6 +81,42 @@ function inicialiarBDD() {
 }
 
 async function main() {
+
+    // 1) Inicializar bdd -- DONE
+    // 2) Preguntas Menu -- DONE
+    // 3) Opciones de Respuesta --  DONE
+    // 4) ACCCION!!!!  -- DONE
+    // 5) Guardar BDD --
+
+
+    // of(Cualquier cosa JS)
+    // from(Promesas)
+    const respuestaBDD$ = rxjs.from(inicialiarBDD());
+
+    respuestaBDD$
+        .pipe(
+            preguntarOpcionesMenu(),
+            opcionesRespuesta(),
+            ejecutarAcccion(),
+            guardarBaseDeDatos()
+        )
+        .subscribe(
+            (data) => {
+                //
+                console.log(data);
+            },
+            (error) => {
+                //
+                console.log(error);
+            },
+            () => {
+                main();
+                console.log('Complete');
+            }
+        )
+
+
+    /*
     try {
         const respuestaInicializarBDD:RespuestaBDD = <RespuestaBDD> await inicialiarBDD();
         const bdd = respuestaInicializarBDD.bdd;
@@ -81,7 +129,7 @@ async function main() {
                 const usuario = await inquirer.prompt(preguntaUsuario);
 
                 // CREAR USUARIO
-                bdd.usuarios.push(usuario);
+                bdd.usuarios.push(usuario); // JS
 
                 const respuestaGuardado = await guardarBDD(bdd);
                 main();
@@ -91,11 +139,13 @@ async function main() {
         }
 
     } catch (e) {
-
+        console.error(e)
     }
+
+    */
 }
 
-function guardarBDD(bdd) {
+function guardarBDD(bdd: BDD) {
     return new Promise(
         (resolve, reject) => {
             fs.writeFile(
@@ -124,13 +174,92 @@ function guardarBDD(bdd) {
 main();
 
 
+function preguntarOpcionesMenu() {
+    return mergeMap( // Respuesta Anterior Observable
+        (respuestaBDD: RespuestaBDD) => {
+
+            return rxjs
+                .from(inquirer.prompt(preguntaMenu))
+                .pipe(
+                    map( // respuesta ant obs
+                        (respuesta: OpcionMenu) => {
+                            respuestaBDD.opcionMenu = respuesta;
+                            return respuestaBDD
+                            // Cualquier cosa JS
+                        }
+                    )
+                );
+
+            // OBSERVABLE!!!!!!!!!!
+        }
+    )
+}
+
+function opcionesRespuesta() {
+    return mergeMap(
+        (respuestaBDD: RespuestaBDD) => {
+            const opcion = respuestaBDD.opcionMenu.opcionMenu;
+            switch (opcion) {
+                case 'Crear':
+                    return rxjs
+                        .from(inquirer.prompt(preguntaUsuario))
+                        .pipe(
+                            map(
+                                (usuario: Usuario) => { // resp ant OBS
+                                    respuestaBDD.usuario = usuario;
+                                    return respuestaBDD;
+                                }
+                            )
+                        );
+                case 'Buscar':
+                    break;
+                case 'Actualizar':
+                    return preguntarIdUsuario(respuestaBDD);
+                case 'Borrar':
+                    break;
+            }
+        }
+    )
+}
+
+function guardarBaseDeDatos() {
+    return mergeMap(// Respuesta del anterior OBS
+        (respuestaBDD: RespuestaBDD) => {
+            // OBS
+            return rxjs.from(guardarBDD(respuestaBDD.bdd))
+        }
+    )
+}
+
+function ejecutarAcccion() {
+    return map( // Respuesta del anterior OBS
+        (respuestaBDD: RespuestaBDD) => {
+            const opcion = respuestaBDD.opcionMenu.opcionMenu;
+            switch (opcion) {
+                case 'Crear':
+                    const usuario = respuestaBDD.usuario;
+                    respuestaBDD.bdd.usuarios.push(usuario);
+                    return respuestaBDD;
+                case 'Actualizar':
+                    const indice = respuestaBDD.indiceUsuario;
+                    console.log('respuestaBDD.bdd.usuarios[indice]', respuestaBDD.bdd.usuarios[indice]);
+                    return respuestaBDD;
+
+            }
+        }
+    )
+}
+
 interface RespuestaBDD {
     mensaje: string;
     bdd: BDD;
+    opcionMenu?: OpcionMenu;
+    usuario?: Usuario;
+    indiceUsuario?: number;
 }
 
 interface BDD {
-    usuarios: Usuario[];
+    usuarios: Usuario[] | any;
     mascotas: Mascota[];
 }
 
@@ -146,7 +275,37 @@ interface Mascota {
     idUsuario: number;
 }
 
+interface OpcionMenu {
+    opcionMenu: 'Crear' | 'Borrar' | 'Buscar' | 'Actualizar';
+}
 
+interface BuscarUsuarioPorId {
+    idUsuario: string;
+}
+
+function preguntarIdUsuario(respuestaBDD: RespuestaBDD) {
+    return rxjs
+        .from(inquirer.prompt(preguntaBuscarUsuario))
+        .pipe(
+            map( // RESP ANT OBS
+                (respuesta: BuscarUsuarioPorId) => {
+                    const indiceUsuario = respuestaBDD.bdd
+                        .usuarios
+                        .findIndex( // -1
+                            (usuario: any) => {
+                                return usuario.id === respuesta.idUsuario
+                            }
+                        );
+                    if (indiceUsuario === -1) {
+                        preguntarIdUsuario(respuestaBDD);
+                    } else {
+                        respuestaBDD.indiceUsuario = indiceUsuario;
+                        return respuestaBDD;
+                    }
+                }
+            )
+        );
+}
 
 
 
