7de207abb74ae0b57b850e0343b8c815308e643a
Adrian Eguez <eadepto@hotmail.com>
2019-01-02 15:14:24 -0500

    typeorm

--- 03-http/videos/src/app.module.ts
+++ 03-http/videos/src/app.module.ts
@@ -2,9 +2,19 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {UsuarioModule} from "./usuario/usuario.module";
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
     imports: [
+        TypeOrmModule
+            .forRoot({
+                type: 'mysql',
+                host: '172.31.104.105',
+                port: 3306,
+                username: 'adrian',
+                password: 'root',
+                database: 'web-adrian',
+            }),
         UsuarioModule
     ], // Modulos
     controllers: [AppController], // Controllers
